7976c2df6bff00e5cc6a4c1a6c312cfe481a198d
Sai Ram <sairam.sadineni@scripbox.com>
2024-12-11 22:24:31 +0530

    Update Dotfiles

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,31 +1,30 @@
 ctrlCmdShortcuts = {
-    {"A", "Alacritty"},
-    {"V", "Visual Studio Code"},
-    {"F", "Firefox"},
-    {"C", "Google Chrome"},
-    {"S", "Slack"},
-    {"N", "Neovide"}
+	{ "A", "Alacritty" },
+	{ "V", "Visual Studio Code" },
+	{ "F", "Firefox" },
+	{ "C", "Google Chrome" },
+	{ "S", "Slack" },
+	{ "N", "Neovide" },
 }
- 
+
 cmdShiftShortcuts = {
-    {"C", "Calendar"},
-    {"M", "Mail"},
-    {"M", "Music"},
-    {"F", "Finder"}
-
+	{ "C", "Calendar" },
+	{ "M", "Mail" },
+	{ "M", "Music" },
+	{ "F", "Finder" },
+	{ "S", "Safari" },
 }
 
-for i,shortcut in ipairs(ctrlCmdShortcuts) do
-    hs.hotkey.bind({"ctrl","cmd"}, shortcut[1], function()
-        hs.application.launchOrFocus(shortcut[2])
-    end)
+for i, shortcut in ipairs(ctrlCmdShortcuts) do
+	hs.hotkey.bind({ "ctrl", "cmd" }, shortcut[1], function()
+		hs.application.launchOrFocus(shortcut[2])
+	end)
 end
 
-
-for i,shortcut in ipairs(cmdShiftShortcuts) do
-  hs.hotkey.bind({"cmd","shift"}, shortcut[1], function()
-        hs.application.launchOrFocus(shortcut[2])
-    end)
+for i, shortcut in ipairs(cmdShiftShortcuts) do
+	hs.hotkey.bind({ "cmd", "shift" }, shortcut[1], function()
+		hs.application.launchOrFocus(shortcut[2])
+	end)
 end
 
 function moveToNextScreen()
@@ -34,11 +33,16 @@ function moveToNextScreen()
 	app:setFullScreen(true)
 end
 
-function makeFullScreen()
+function maximizeScreen()
 	local app = hs.window.focusedWindow()
-	app:setFullScreen(true)
+	app:maximize(true)
 end
 
+function toggleFullScreen()
+	local app = hs.window.focusedWindow()
+	app:toggleFullScreen(true)
+end
 
-hs.hotkey.bind({"cmd"}, "n", moveToNextScreen)
-hs.hotkey.bind({"cmd"}, "l", makeFullScreen)
+hs.hotkey.bind({ "cmd" }, "n", moveToNextScreen)
+hs.hotkey.bind({ "cmd" }, "l", maximizeScreen)
+hs.hotkey.bind({ "cmd" }, "m", toggleFullScreen)
